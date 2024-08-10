daacee99ba25dbcda72d25d9100332ce92e82ecc
Panagiotis <ics20044@uom.edu.gr>
2024-08-10 18:29:42 +0300

    Initial commit with Spring Boot API

---
+++ src/test/java/com/devskill/devskill_api/DevskillApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devskill.devskill_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevskillApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
