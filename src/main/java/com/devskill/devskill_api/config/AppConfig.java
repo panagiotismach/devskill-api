001f122d3d85243453b4257b9c348ae340e525d4
Panagiotis <ics20044@uom.edu.gr>
2024-09-28 16:57:21 +0300

    Insert app config into configuration

--- src/main/java/com/devskill/devskill_api/config/AppConfig.java
+++ src/main/java/com/devskill/devskill_api/config/AppConfig.java
@@ -1,4 +1,4 @@
-package com.devskill.devskill_api;
+package com.devskill.devskill_api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
